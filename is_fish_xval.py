f53d469206752bf92c1b199488a22a6555c42553
Joel Malchiondo <joelm@yelp.com>
2016-12-16 11:18:25 -0800

    experimenting

--- is_fish_xval.py
+++ is_fish_xval.py
@@ -18,6 +18,11 @@ import pickle
 from PIL import Image
 import time
 import xgboost
+from sklearn.metrics import f1_score, recall_score
+from sklearn.metrics import fbeta_score, make_scorer
+import random
+
+random.seed(10)
 
 
 #1)understand what inception layers mean and # of features as each layers
@@ -26,7 +31,6 @@ import xgboost
 #4)When submission train on all
 
 def main():
-    cmd_opts = _parse_args()
 
     plt.figure(figsize=(17,10))
     model_dir = 'imagenet'
@@ -79,101 +83,33 @@ def main():
     X_train= features
     y_train= labels
     X_is_fish_train= features_is_fish
-    y_is_fish_train=labels_is_fish
-
-    print("Fitting SVM for is fish model")
+    y_is_fish_train= map(lambda x: int(x!='FISH'), labels_is_fish)
     clf_is_fish = svm.SVC(
         kernel='poly', 
-        C=10, 
-        gamma=0.1, 
         probability=True, 
         random_state=324
-    )
-    clf_is_fish.fit(X_is_fish_train, y_is_fish_train)
-    is_fish_train_pred = clf_is_fish.predict(X_is_fish_train)
-    print("Adding is fish model features to main model")
-    print(len(X_train[0]))
-    new_train=[]
-    for i, row in enumerate(X_train):
-        new_train.append(np.append(arr=row, values=int(is_fish_train_pred[i] == "FISH")))
-    print ("Using XGBoost")
-    print(len(new_train[0]))
-    X_train= np.array(new_train)
-    clf= xgboost.XGBClassifier()
-    print("Fitting Last Layer")
-    clf.fit(X_train, y_train)
-   
-    print("Creating Test Data from Folders in {}".format(test_image_dir))
-    test_data=create_test_data(test_image_dir)
-    if not os.path.isfile('test-features'):
-        print('Extracting Features')
-        test_features,_ = extract_features(
-            model_dir=model_dir,
-            data=test_data
-        )
-        print('Dumping Test-Features')
-        pickle.dump(test_features, open('test-features', 'wb'))
-    else:
-        print('Found test features. Loading...')
-        test_features = pickle.load(open('test-features'))
-    print('Predict is fish features for test')
-    is_fish_test_pred= clf_is_fish.predict(test_features)
-    print("Adding is fish model test features")
-    new_test=[]
-    for i, row in enumerate(test_features):
-        new_test.append(np.append(row, int(is_fish_train_pred[i] == "FISH")))
-    test_features= np.array(new_test)
-    print('Predicting Test')
-    test_pred = clf.predict(test_features)
-    csv_headers = [
-        fish_folder_map[c]
-        for c in clf.classes_
-    ]
-    print('Predicting Probabilities')
-    test_proba = clf.predict_proba(test_features)
-    test_df = pd.DataFrame(
-        data=test_proba,
-        columns=csv_headers
-    )
-    test_df.loc[:,'image'] = [
-        os.path.split(img_data['img'])[1] #Get the tail portion file.end
-        for img_data in test_data
-    ]
-    test_df.to_csv(
-        path_or_buf='submission.csv',
-        sep=',',
-        index=False
-    )
-    badCount=0
-    for i in xrange(len(test_pred)):
-        maxI=np.where(test_proba[i]==max(test_proba[i]))[0][0]
-        if test_pred[i] != clf.classes_[maxI]:
-            badCount+=1
-    print("Bad count of: {}".format(badCount))
 
+    )
+    search_clf= model_selection.GridSearchCV(
+        estimator=clf_is_fish,
+        param_grid={
+                'C':[0.001],
+                'degree': [2],
+            },
+        n_jobs=3,
+        cv=3,
+        refit=True,
+        verbose=3,
+        scoring=make_scorer(f1_score),
+    )
+    search_clf.fit(X_is_fish_train, y_is_fish_train)
+    print('Best score of: {score} params:\n{params}'.format(
+        score=search_clf.best_estimator_,
+        params=search_clf.best_params_
+    ))
     print("Bye")
 
 
-def _parse_args():
-    """Parse command line arguments"""
-    args = sys.argv[1:]
-    cmd_parser = argparse.ArgumentParser()
-    cmd_parser.add_argument(
-        '--produce-sub',
-        dest='produce_sub',
-        help='Produce submision file',
-        default=False,
-        action='store_true',
-    )
-    cmd_parser.add_argument(
-        '--search-cv',
-        dest='search_cv',
-        help='Perform Search of parameters',
-        default=False,
-        action='store_true',
-    )
-    cmd_opts = cmd_parser.parse_args(args=args)
-    return cmd_opts
 
 
 def create_train_data(images_dir, fish_folder_map, fish_types=None):
